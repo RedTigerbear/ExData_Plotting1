ba96e3c35553e9a3646da94d357aed9c66a68bd4
RedTigerbear <RedTigerbear@users.noreply.github.com>
2015-05-08 15:41:52 +0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -13,5 +13,7 @@ rm(household_power_consumption)
 
 ## Plot 2
 plot(data$Global_active_power~data$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+## Save to PNG
 dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
